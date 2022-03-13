81eb651baaf473227c780e83c77518045ea16d0e
SergPerm <mohin@list.ru>
2022-03-13 18:25:48 +0500

    8. HTML form. Создание кандидата. [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.Store;
 
 @Controller
@@ -13,5 +14,11 @@ public class CandidateController {
     public String candidates(Model model) {
         model.addAttribute("candidates", store.findAllCandidates());
         return "candidates";
+    }
+
+    @GetMapping("/addCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("addCandidate", new Candidate(0, "Задайте имя"));
+        return "addCandidate";
     }
 }
