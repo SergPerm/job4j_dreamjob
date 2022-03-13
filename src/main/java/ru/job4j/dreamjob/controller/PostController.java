e857f20881abfeb7c19ac520f9810c20559ba822
SergPerm <mohin@list.ru>
2022-03-13 18:11:04 +0500

    7. HTML form. Создание вакансии. [#504844]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.Store;
 
 @Controller
@@ -15,4 +16,11 @@ public class PostController {
         model.addAttribute("posts", store.findAllPosts());
         return "posts";
     }
+
+    @GetMapping("/addPost")
+    public String addPost(Model model) {
+        model.addAttribute("addPost", new Post(0, "Задайте имя"));
+        return "addPost";
+    }
+
 }
