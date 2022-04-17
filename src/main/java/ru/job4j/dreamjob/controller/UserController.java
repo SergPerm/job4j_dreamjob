6a98beb5f088fc599c4f74b07de269439649970b
SergPerm <mohin@list.ru>
2022-04-17 15:08:52 +0500

    Многопоточность в базе данных [#504860]+

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -33,7 +33,6 @@ public class UserController {
                                @ModelAttribute User user) {
         Optional<User> regUser = userService.createUser(user);
         if (regUser.isEmpty()) {
-            model.addAttribute("msg", "Пользователь с такой почтой уже существует");
             return "redirect:/fail";
         }
         return "redirect:/success";
@@ -46,6 +45,7 @@ public class UserController {
 
     @GetMapping("/fail")
     public String registrationFail(Model model) {
+        model.addAttribute("msg", "Пользователь с такой почтой уже существует");
         return "registrationFail";
     }
 }
